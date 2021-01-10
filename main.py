c90e78603d51fdee432930f13cbf167099aa0185
Intron014 <68110374+Intron014@users.noreply.github.com>
2021-01-10 15:04:54 +0100

    Main "bones" completed

---
+++ main.py
@@ -0,0 +1,25 @@
+import discord #Da main stuff
+import logging #Da logging stuff
+
+
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run('Nzk3NzgwODk3ODU2MjI1Mjkx.X_rdXA.uWDXpoiK1HMX836H0LpEIjqDdwY')
